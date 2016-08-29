2629e53ffbdbf814943625d53380a82344b76f86
jaeoheeail <joelfooet@gmail.com>
2016-08-29 21:10:50 +0800

    Use .toString() method to get value

--- test/java/seedu/addressbook/parser/ParserTest.java
+++ test/java/seedu/addressbook/parser/ParserTest.java
@@ -67,7 +67,7 @@ public class ParserTest {
     }
 
     /**
-     * Test ingle index argument commands
+     * Test single index argument commands
      */
     
     @Test
@@ -266,7 +266,7 @@ public class ParserTest {
                 + person.getName().fullName
                 + (person.getPhone().isPrivate() ? " pp/" : " p/") + person.getPhone().value
                 + (person.getEmail().isPrivate() ? " pe/" : " e/") + person.getEmail().value
-                + (person.getAddress().isPrivate() ? " pa/" : " a/") + person.getAddress().value;
+                + (person.getAddress().isPrivate() ? " pa/" : " a/") + person.getAddress().toString();
         for (Tag tag : person.getTags()) {
             addCommand += " t/" + tag.tagName;
         }
