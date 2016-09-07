230c19bc3a0686b9780c6f8bb8668239d111155c
jaeoheeail <joelfooet@gmail.com>
2016-09-07 10:16:34 +0800

    Add test case 2

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -4,12 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class NameTest {
 
 	@Test
-	public void testIsSimilar() {
+	public void testIsSimilar() throws IllegalValueException {
 		// null name
 		assertFalse(Name.isSimilar(null));
+		
+		// random name
+		assertFalse(Name.isSimilar(new Name("aaa")));
 	}
 
 }
