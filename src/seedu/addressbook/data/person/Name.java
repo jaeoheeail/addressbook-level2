d68eb3e181344e8735893430c9915df84bea1ee9
jaeoheeail <joelfooet@gmail.com>
2016-09-11 13:48:42 +0800

    Change modifier from public to private

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -11,78 +11,78 @@ import java.util.List;
  */
 public class Name {
 
-	public static final String EXAMPLE = "John Doe";
-	public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphabetic characters";
-	public static final String NAME_VALIDATION_REGEX = "[\\p{Alpha} ]+";
+    public static final String EXAMPLE = "John Doe";
+    private static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphabetic characters";
+    private static final String NAME_VALIDATION_REGEX = "[\\p{Alpha} ]+";
 
-	public final String fullName;
+    public final String fullName;
 
-	/**
-	 * Validates given name.
-	 *
-	 * @throws IllegalValueException if given name string is invalid.
-	 */
-	public Name(String name) throws IllegalValueException {
-		name = name.trim();
-		if (!isValidName(name)) {
-			throw new IllegalValueException(MESSAGE_NAME_CONSTRAINTS);
-		}
-		this.fullName = name;
-	}
+    /**
+     * Validates given name.
+     *
+     * @throws IllegalValueException if given name string is invalid.
+     */
+    public Name(String name) throws IllegalValueException {
+        name = name.trim();
+        if (!isValidName(name)) {
+            throw new IllegalValueException(MESSAGE_NAME_CONSTRAINTS);
+        }
+        this.fullName = name;
+    }
 
-	/**
-	 * Returns true if a given string is a valid person name.
-	 */
-	public static boolean isValidName(String test) {
-		return test.matches(NAME_VALIDATION_REGEX);
-	}
+    /**
+     * Returns true if a given string is a valid person name.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(NAME_VALIDATION_REGEX);
+    }
 
-	/**
-	 * Retrieves a listing of every word in the name, in order.
-	 */
-	public List<String> getWordsInName() {
-		return Arrays.asList(fullName.split("\\s+"));
-	}
+    /**
+     * Retrieves a listing of every word in the name, in order.
+     */
+    public List<String> getWordsInName() {
+        return Arrays.asList(fullName.split("\\s+"));
+    }
 
-	@Override
-	public String toString() {
-		return fullName;
-	}
+    @Override
+    public String toString() {
+        return fullName;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		return other == this // short circuit if same object
-				|| (other instanceof Name // instanceof handles nulls
-						&& this.fullName.equals(((Name) other).fullName)); // state check
-	}
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Name // instanceof handles nulls
+                        && this.fullName.equals(((Name) other).fullName)); // state check
+    }
 
-	@Override
-	public int hashCode() {
-		return fullName.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return fullName.hashCode();
+    }
 
-	/**
-	 * Returns true of the other name is very similar to this name.
-	 * Two names are considered similar if:
-	 * 1. They are both equal
-	 * 2. Other name is a subset of this name
-	 */
-	public boolean isSimilar(Name other) {
-		if (other != null) {
-			if (this.equals(other)) {
-				return true;
-			} else  {
-				String[] splitOther = other.fullName.toLowerCase().split(" ");
-				boolean result = false;
-				for (String o: splitOther) {
-					if (this.fullName.toLowerCase().contains(o)) {
-						result = true;
-					}
-				}
-				return result;
-			}
-		}
-		return false;
-	}
+    /**
+     * Returns true of the other name is very similar to this name.
+     * Two names are considered similar if:
+     * 1. They are both equal
+     * 2. Other name is a subset of this name
+     */
+    public boolean isSimilar(Name other) {
+        if (other != null) {
+            if (this.equals(other)) {
+                return true;
+            } else  {
+                String[] splitOther = other.fullName.toLowerCase().split(" ");
+                boolean result = false;
+                for (String o: splitOther) {
+                    if (this.fullName.toLowerCase().contains(o)) {
+                        result = true;
+                    }
+                }
+                return result;
+            }
+        }
+        return false;
+    }
 
 }
